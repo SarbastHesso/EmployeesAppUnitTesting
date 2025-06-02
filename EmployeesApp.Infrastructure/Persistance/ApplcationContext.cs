cf0736b94c36d3d9304def332b34722934571111
albertito1985 <anulo@live.se>
2025-06-02 14:00:40 +0200

    Added Context

---
+++ EmployeesApp.Infrastructure/Persistance/ApplcationContext.cs
@@ -0,0 +1,57 @@
+﻿using EmployeesApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace EmployeesApp.Infrastructure.Persistance
+{
+    // Denna konstruktor krävs för att konfigurationen i Program.cs ska fungera
+    public class ApplicationContext(DbContextOptions<ApplicationContext> options)
+    : DbContext(options)
+    {
+        // Exponerar våra entiteter via properties av typen DbSet<T>
+        public DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee()
+                {
+                    Id = 562,
+                    Name = "Anders Hejlsberg",
+                    Email = "Anders.Hejlsberg@outlook.com",
+                },
+                new Employee()
+                {
+                    Id = 62,
+                    Name = "Kathleen Dollard",
+                    Email = "k.d@outlook.com",
+                },
+                new Employee()
+                {
+                    Id = 15662,
+                    Name = "Mads Torgersen",
+                    Email = "Admin.Torgersen@outlook.com",
+                },
+                new Employee()
+                {
+                    Id = 52,
+                    Name = "Scott Hanselman",
+                    Email = "s.h@outlook.com",
+                },
+                new Employee()
+                {
+                    Id = 563,
+                    Name = "Jon Skeet",
+                    Email = "j.s@outlook.com",
+                }
+            );
+        }
+    }
+}
